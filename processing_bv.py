152285d87d1eee9f112c90624329bd1a77911fd3
JMZ-kk <65991418+JMZ-kk@users.noreply.github.com>
2024-09-13 22:56:10 -0400

    Add files via upload

--- processing_bv.py
+++ processing_bv.py
@@ -12,6 +12,36 @@ import os
 import glob
 import pickle
 
+def get_bv_data(sched):
+    time_series = {}
+    total_power_trace = {}
+    for time, instruction in sched.instructions:
+        if isinstance(instruction, qiskit.pulse.Play):
+            pulse_waveform = instruction.pulse
+            channel = instruction.channel
+            if isinstance(pulse_waveform, qiskit.pulse.library.ScalableSymbolicPulse):
+                amplitude = pulse_waveform.get_waveform().samples
+            else:
+                amplitude = pulse_waveform.samples
+            power = np.real(amplitude)**2 + np.imag(amplitude)**2
+            start_time = time
+            times = np.arange(start_time, start_time + len(power))
+            if channel not in time_series:
+                time_series[channel] = (times, power)
+            else:
+                existing_times, existing_power = time_series[channel]
+                time_series[channel] = (np.concatenate([existing_times, times]),
+                                        np.concatenate([existing_power, power]))
+
+            for t, p in zip(times, power):
+                if t in total_power_trace:
+                    total_power_trace[t] += p
+                else:
+                    total_power_trace[t] = p
+    total_times = np.array(sorted(total_power_trace.keys()))
+    total_power = np.array([total_power_trace[t] for t in total_times])
+    return total_power
+
 circuit_list= {}
 max_nq=6
 for nq in range(1,max_nq+1):
@@ -25,13 +55,26 @@ for json_file in json_files:
         total_power_trace = data['total_power_trace']
         circuit_list[int(tp[1])].append(total_power_trace)
 
-try_times = 300
+try_times = 30
 corrects=np.zeros(max_nq)
 for nq in range(1,max_nq+1):
     base=circuit_list[nq]
     for j in range(try_times):
         ans = random.randint(0,len(base)-1)
-        new_power_trace = np.array(base[ans])
+        s = format(ans, '0' + str(nq) + 'b')
+        n = len(s)
+        qc = QuantumCircuit(n + 1, n)
+        qc.x(n)
+        qc.h(range(n + 1))
+        for ii, yesno in enumerate(reversed(s)):
+            if yesno == '1':
+                qc.cx(ii, n)
+        qc.h(range(n + 1))
+        qc.measure(range(n), range(n))
+        simulator = FakeCasablancaV2()
+        circ = transpile(qc, simulator)
+        sched = schedule(circ, simulator)
+        new_power_trace = list(get_bv_data(sched))
         guess = 0
         min_dis=999999999
         for i in range(len(base)):
